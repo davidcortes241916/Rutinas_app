bcd89f606721025601a36289ebcf9ee8a954928f
David Cortes <alexis241916@gmail.com>
2024-04-23 19:30:51 -0500

    se creo un nuevo valor en el modelo llamado email

--- login_app/models.py
+++ login_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Registro(models.Model):
+    email_usuario= models.CharField(max_length=100, null= False, default="")
+    nombre_usuario= models.CharField(max_length= 80, null= False)
+    fecha_nacimiento= models.DateField(null= False)
+    contraseña= models.CharField(max_length=100, null= False)
+    imagen_perfil= models.ImageField(null= True)
+
+    def __str__(self):
+        return self.nombre_usuario
