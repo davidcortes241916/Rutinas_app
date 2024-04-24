1bd6f2d478d00110e2a86daf20ba50e55a6bb74f
David Cortes <alexis241916@gmail.com>
2024-04-23 19:32:06 -0500

    se prepara el entorno para el crud de login

--- login_app/views.py
+++ login_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Registro
 
 def index(request):
     return render(request, "./login.html", {})
+
+def registrar_usuario(request):
+    pass
