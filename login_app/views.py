1454eccc359c8ef022ce29abf34898e4b2db8881
David Cortes <alexis241916@gmail.com>
2024-04-26 20:25:51 -0500

    se agregaron 2 funciones nuevas y se completo la de registro, se debe modificar registro para agregar unas cuantas funciones, entre ellas que valide si existe el email, y hacer el codigo mas limpio

--- login_app/views.py
+++ login_app/views.py
@@ -1,9 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib import messages
 from .models import Registro
 
 def index(request):
-    return render(request, "./login.html", {})
+    return render(request, "./login.html")
+
+def home(request):
+    return render(request, "./home.html")
 
 def registrar_usuario(request):
-    pass
+    if request.method != "POST":
+        return render(request, "./login.html") 
+
+    else:  
+        try:
+            if request.POST["password"] == request.POST["password2"]:
+                user = Registro.objects.create(
+                    email_usuario= request.POST["email_usuario"],
+                    nombre_usuario= request.POST["nombre_usuario"],
+                    fecha_nacimiento= request.POST["fecha_nacimiento"],
+                    password= request.POST["password"]
+                )
+                user.save()
+                return redirect("home")  
+            else:
+                return HttpResponse("Las contraseñas no coinciden")
+        except Exception as e:
+            print(f"Error registering user: {e}") 
+            return HttpResponse("Ocurrió un error durante el registro.")
+            
+            
