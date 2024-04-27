a447666b46551fb0b742c8abccdd3917cba4f219
David Cortes <alexis241916@gmail.com>
2024-04-26 20:23:54 -0500

    se generaron modificaciones en la url principal para el funcionamiento del login

--- rutina_app/urls.py
+++ rutina_app/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("login_app.urls"), name= "login"),
+    path('', include("login_app.urls"), name= "index"),
 ]
